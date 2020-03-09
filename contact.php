56043bf375e3501b7617c8cf1fa50061af0d829b
Danilo de Nadai Sicari <danilons@ciandt.com>
2020-03-09 20:33:55 -0300

    added contact desktop

---
+++ contact.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <?php include_once("templates/head.php"); ?>
+        <link rel="stylesheet" href="./assets/css/contact.css">
+    </head>
+    
+    <?php include_once("widgets/navbar.php"); ?>
+
+    <body>
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-sm-2 col-lg-1"></div>
+                <div class="col-sm-5 col-lg-10 title-cover-home">
+                    You're gonna need a bigger boat.                                 
+                </div>
+                <div class="col-sm-5 col-lg-1"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-1 col-sm-1"></div>
+                <div class="subtitle-cover-home col-lg-10 col-sm-10">
+                    1975, Jaws
+                </div>
+                <div class="col-lg-1 col-sm-1"></div>
+            </div>
+
+            <div class="row row-social-media">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-2">
+                    <span class="social-media">gmail<span><br>
+                    <span class="contact-social-media">denadai.sicari<span>                                
+                </div>
+                <div class="col-lg-2">
+                    <span class="social-media">github<span><br>
+                    <span class="contact-social-media">dsicari<span>                                
+                </div>
+                <div class="col-lg-2">
+                    <span class="social-media">linkedin<span><br>
+                    <span class="contact-social-media">dsicarii<span>                                
+                </div>
+                <div class="col-lg-2">
+                    <span class="social-media">twitter<span><br>
+                    <span class="contact-social-media">dsicariii<span>                                
+                </div>
+                <div class="col-lg-2">
+                    <img class="shark-icon" src="assets/icon/shark.svg">                                
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+
+        </div>        
+    </body>
+
+</html>
